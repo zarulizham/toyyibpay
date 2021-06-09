a33100d616e77a2ff02d8fecd290f23a0ec47f0f
Zarul Zubir <zarul.izham@gmail.com>
2021-06-09 20:45:10 +0800

    Add payment link atttribute

--- src/Models/ToyyibPayBill.php
+++ src/Models/ToyyibPayBill.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class ToyyibPayBill extends Model
 {
     protected $guarded = [];
+
+    public function getPaymentLinkAttribute()
+    {
+        if (config('toyyibpay.staging')) {
+            return 'https://dev.toyyibpay.com/'.$this->bill_code;
+        } else {
+            return 'https://toyyibpay.com/'.$this->bill_code;
+        }
+    }
 }
